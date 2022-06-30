1d2d002e84ef183ab0fa4148770fb22a489bfa53
ManashJKonwar <rickykonwar@gmail.com>
2022-06-30 23:50:39 +0530

    Enhancement: Scripts_associated_with_extarcting_nlp_based_information_from_datasets_and_generating_graph_db
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ GenerateGraph/graph_generator.py
@@ -0,0 +1,309 @@
+import numpy as np
+import logging
+import pyTigerGraph as tg
+import json
+import re #regex
+import datetime
+
+class GraphGenerator:
+    """
+    This class is to connect to TigerGraph and create the necessary node or relationship, and check if one of them exists already.
+    """
+
+    def __init__(self, config, logger) -> None:
+        """
+        constructor method. Config and Logger instances have to be passed on from the caller.
+        """
+        self.config = config
+        self.logger = logger
+
+        self.host_name = self.config['GraphSettings']['HostName']
+        self.user_name = self.config['GraphSettings']['UserName']
+        self.password = self.config['GraphSettings']['Password']
+        self.graph_name = self.config['GraphSettings']['GraphName']
+        self.should_create_graph = (self.config['GraphSettings']['CreateGraph'] == 'True')
+        self.secret = ''
+        self.api_token = ''
+        self.graph_auto_created = False
+
+        self.connection = tg.TigerGraphConnection(host=self.host_name, username=self.user_name, password=self.password, graphname=self.graph_name, gsqlVersion="3.5.0", useCert=True)
+
+        self.logger.info("GraphGenerator initialized.")
+
+
+    def initialize_token(self):
+        self.logger.info("GraphGenerator connection initialized. Creating secret.")
+        # For the below statements to execute the graph should be created and available
+        # create the secret
+        self.secret = self.config['GraphSettings']['Secret'] 
+        if str.strip(self.secret) == '' or self.graph_auto_created == True:
+            self.secret = self.connection.createSecret()
+
+        self.api_token = self.config['GraphSettings']['ApiToken'] 
+        if str.strip(self.api_token) == '' or self.graph_auto_created == True:
+            self.api_token =  self.connection.getToken(self.secret, setToken=True)
+
+
+    def use_graph(self):
+        """
+        This method executes use graph statement. Default graph name is fetched from the config
+        """
+        self.logger.info("use graph is being called.")
+        full_query = str.format("USE GRAPH {}", self.graph_name)
+        result = self.connection.gsql(full_query)
+
+        result = str.lower(result)
+        not_exists_message = str.lower(str.format("Graph '{}' does not exist.", self.graph_name))
+        using_graph_message = str.lower("Using graph")
+
+        if (not_exists_message in result) == True:
+            # relying on string result is not a standard way of doing. Need to figure out another way from API if one exists.
+            self.logger.info("Graph does not exist. Calling create graph.")
+            self.create_graph()
+        elif (using_graph_message in result) == False:
+            # relying on string result is not a standard way of doing. Need to figure out another way from API if one exists.
+            raise Exception("ERR: Use graph query execution failed.")
+        else:
+            self.logger.info("Graph exists. Using it.")
+
+
+    def create_graph(self):
+        """
+        This method executes create graph statement, if the configuration is true. Default graph name is fetched from the config.
+        """
+        self.logger.info("create graph is being called.")
+        full_query = str.format("CREATE GRAPH {} ()", self.graph_name)
+        if self.should_create_graph:
+            #execute create graph
+            result = self.connection.gsql(full_query)
+
+            success_message = str.lower(str.format('The graph {} is created', self.graph_name))
+            result = str.lower(result)
+
+            if (success_message in result) == False:
+                raise Exception("ERR: Create graph query execution failed.")
+            
+            self.graph_auto_created = True
+            self.logger.info("create graph is completed.")
+        else:
+            self.logger.info("create graph is not executed because it is configured not to create.")
+            raise Exception("ERR: Create graph is not executed because it is configured not to create.")
+    
+
+    def create_nodes_schema(self, node_infos):
+        """
+        This method creates the initial nodes (vertices) with the given list of node_infos
+        """
+        self.logger.info("create_nodes_schema is called.")
+        self.logger.info("getting existing vertex types.")
+
+        #existing_node_types = self.connection.getVertexTypes(force=True)
+        existing_node_types = self.get_existing_vertex_types_gsql()
+        existing_node_types = [str.lower(n) for n in existing_node_types]
+
+        query_list = []
+        new_node_available = False
+        to_be_added = []
+
+        for node_info in node_infos:
+            #skip this if it already exists.
+            if str.lower(node_info['name']) in existing_node_types:
+                continue
+            query_list.append(str.format('CREATE VERTEX {}(PRIMARY_ID id STRING, name STRING) WITH STATS="OUTDEGREE_BY_EDGETYPE", PRIMARY_ID_AS_ATTRIBUTE="true"', node_info['name']))
+            new_node_available = True
+            to_be_added.append(node_info)
+
+        full_query = "\n".join(query_list)
+        full_query = "USE GLOBAL" + "\n" + full_query
+        self.logger.info("creating vertex types globally.")
+        result = self.connection.gsql(full_query)
+
+        self.logger.info(str.format("result: {}", result))
+
+        if new_node_available:
+            #adding the created nodes into the graph
+            self.logger.info("adding new nodes to graph's schema.")
+            self.add_nodes_to_graph_schema(to_be_added)
+
+
+    def create_relationships_schema(self, relationship_infos):
+        """
+        This method creates the initial relationships (edges) with the given list of relationship_infos.
+        NOTE that all nodes are created using wildcard option. 
+        """
+        self.logger.info("create_relationships_schema is called.")
+        self.logger.info("getting existing edge types.")
+
+        #existing_relationship_types = self.connection.getEdgeTypes(force=True)
+        existing_relationship_types = self.get_existing_edges_types_gsql()
+        existing_relationship_types = [str.lower(r) for r in existing_relationship_types]
+
+        query_list = []
+        new_relationship_available = False
+        to_be_added = []
+
+        for relationship_info in relationship_infos:
+            #skip this if it already exists.
+            relationship_name = GraphGenerator.get_relationship_name(relationship_info['name'])
+            if str.lower(relationship_name) in existing_relationship_types:
+                continue
+            query_list.append(str.format('CREATE DIRECTED EDGE {} (FROM *, TO *, label STRING, happened DATETIME, month UINT, year UINT) WITH REVERSE_EDGE="reverse_{}"', relationship_name, relationship_name))
+            ## CAUTION: wildcard edges creation. Will only consider the vertex types at the time of execution. Future vertices are not considered. Create the master data vertices before this action, if any.
+            ## CAUTION: 'r_' is prefixed with the edge name as in 'r_NAME' to avoid the names clashing with reserved keywords.
+            new_relationship_available = True
+            to_be_added.append(relationship_info)
+
+        full_query = "\n".join(query_list)
+        full_query = "USE GLOBAL" + "\n" + full_query
+        self.logger.info("creating relationship types globally.")
+        result = self.connection.gsql(full_query)
+
+        self.logger.info(str.format("result: {}", result))
+
+        if new_relationship_available:
+            #adding the created relationships into the graph
+            self.logger.info("adding new relationships to graph's schema.")
+            self.add_relationships_to_graph_schema(to_be_added)
+
+
+    def add_nodes_to_graph_schema(self, node_infos):
+        """
+        This method ports all the nodes (vertexes) created using create_nodes method to a particular graph
+        """
+        self.logger.info("add_nodes_to_graph_schema is called.")
+
+        query_list = []
+        for node_info in node_infos:
+            query_list.append(str.format('ADD VERTEX {} TO GRAPH {};', node_info['name'], self.graph_name))
+
+        job_suffix = datetime.datetime.now().strftime("_%Y_%m_%d_%H%M%S")
+
+        add_nodes_query = "\n".join(query_list)
+        add_nodes_query = """
+        USE GLOBAL
+        CREATE GLOBAL SCHEMA_CHANGE JOB add_vertices_to_"""+ self.graph_name + job_suffix +""" {
+        """ + add_nodes_query + """
+        }
+        RUN GLOBAL SCHEMA_CHANGE JOB add_vertices_to_"""+ self.graph_name + job_suffix +"""
+        """
+        result = self.connection.gsql(add_nodes_query)
+
+        self.logger.info(str.format("result: {}", result))
+        
+
+
+    def get_existing_vertex_types_gsql(self):
+        """
+        This gets the list of existing vertices using GSQL and parses the output using regular expression
+        """
+        self.logger.info("get_existing_vertices_gsql is called.")
+
+        query = "SHOW VERTEX **"
+
+        existing_vertices_result = self.connection.gsql(query)
+
+        self.logger.info(str.format("result: {}", existing_vertices_result))
+
+        existing_vertices = re.findall("VERTEX (.+)\(.+\)", existing_vertices_result)
+        return existing_vertices
+
+
+    def get_existing_edges_types_gsql(self):
+        """
+        This gets the list of existing edges using GSQL and parses the output using regular expression
+        """
+        self.logger.info("get_existing_edges_gsql is called.")
+
+        query = "SHOW EDGE **"
+
+        existing_edges_result = self.connection.gsql(query)
+        existing_edges = re.findall("DIRECTED EDGE (r_.+)\(.+\)", existing_edges_result)
+        return existing_edges
+
+
+    def add_relationships_to_graph_schema(self, relationship_infos):
+        """
+        This method ports all the relationships (edges) created using create_relationships method to a particular graph
+        """
+        self.logger.info("add_relationships_to_graph_schema is called.")
+
+        query_list = []
+        for relationship_info in relationship_infos:
+            relationship_name = GraphGenerator.get_relationship_name(relationship_info['name'])
+            query_list.append(str.format('ADD EDGE {} TO GRAPH {}; ADD EDGE reverse_{} TO GRAPH {};', relationship_name, self.graph_name, relationship_name, self.graph_name))
+
+        job_suffix = datetime.datetime.now().strftime("_%Y_%m_%d_%H%M%S")
+
+        add_relationships_query = "\n".join(query_list)
+        add_relationships_query = """
+        USE GLOBAL
+        CREATE GLOBAL SCHEMA_CHANGE JOB add_edges_to_"""+ self.graph_name + job_suffix +""" {
+        """ + add_relationships_query + """
+        }
+        RUN GLOBAL SCHEMA_CHANGE JOB add_edges_to_"""+ self.graph_name + job_suffix +"""
+        """
+        result = self.connection.gsql(add_relationships_query)
+
+        self.logger.info(str.format("result: {}", result))
+
+
+    def get_relationship_name(verb_token):
+        """
+        returns the relationship name from verb token
+        """
+        return "r_" + verb_token
+        
+
+    def add_node_to_graph(self, node_token, node_type):
+        """
+        This method will add new nodes to the graph
+        if exists already, it will just return the id
+        """
+        if(self.api_token == ''):
+            self.initialize_token()
+
+        existing_nodes = self.connection.getVertices(node_type, "", str.format('name="{}"', node_token['token']))
+        node_id = ""
+        if len(existing_nodes) > 0:
+            node_id = existing_nodes[0]['v_id']
+        else:
+            node_count = self.connection.getVertexCount("*").values() #cannot rely on this fully. If we do async methods / do inserts from multiple clients, this will run into concurrency issues 
+            node_id = sum(node_count) + 1 #generated with count for now. TODO: How else to reliably generate this id?
+            self.connection.upsertVertex(node_type, node_id, { "name": node_token['token']}) 
+
+        return node_id
+
+    def add_relationship_to_graph(self, from_node_type, from_node, relationship, to_node_type, to_node, attributes=None):
+        """
+        This method will add new relationships between from_node and to_node to the graph 
+        """
+        if(self.api_token == ''):
+            self.initialize_token()
+
+        self.connection.upsertEdge(from_node_type, from_node, relationship, to_node_type, to_node, attributes)
+
+
+    def drop_all(self):
+        """
+        WARNING: destroyer method. Should not be used unless you want to start again.
+        clears all the schema.
+        """
+        self.logger.info("clearing off the schema (drop all).")
+        self.connection.gsql('''
+        USE GLOBAL
+        DROP ALL
+        ''')
+
+    def setup_schema(self, node_infos, relationship_infos):
+        """
+        Sets up schema with Graph for given node_infos and relationship_infos
+        """
+        self.logger.info("setup_schema is called.")
+
+        #check and use / create graph if necessary before creating the schema.
+        self.use_graph()
+
+        self.create_nodes_schema(node_infos)
+        self.create_relationships_schema(relationship_infos)
+
+
---
+++ GenerateGraph/input_data_handler.py
@@ -0,0 +1,226 @@
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import logging
+
+from domain_words_extractor import DomainWordsExtractor
+from entity_extractor import EntityExtractor
+from graph_generator import GraphGenerator
+from pattern_finder import PatternFinder
+from pos_extractor import PosExtractor
+
+from os import path
+
+class InputDataHandler : 
+    """
+    This class is to handle the input data file and call the necessary downstream actions on it.
+    Extracts Entities on a given text, Extracts Parts-of-speech on the given text, and extracts domain words.
+    Then compares them and forms the necessary relationships before calling the graph generation.
+    """
+
+    def __init__(self, config, logger, entity_extractor : EntityExtractor, pos_extractor : PosExtractor, domain_words_extractor: DomainWordsExtractor, pattern_finder: PatternFinder, graph_generator: GraphGenerator) -> None:
+        """
+        constructor method. Config, Logger, Entity Extracor, Pos Extractor, and Domain Words Extractor instances have to be passed on from the caller.
+        """
+        self.config = config
+        self.logger = logger
+
+        self.entity_extractor = entity_extractor
+        self.pos_extractor = pos_extractor
+        self.domain_words_extractor = domain_words_extractor
+        self.generate_domain_words = (self.config['InputDataSettings']['GenerateDomainWords'] == 'True')
+        self.pattern_finder = pattern_finder
+        self.graph_generator = graph_generator
+        self.nodes_list = ['Organization','Person','Object','Location']
+        self.domain_verbs_set = set()
+        self.domain_nouns_set = set()
+        self.content_title = config['InputDataSettings']['ContentTitle']
+        self.date_title = config['InputDataSettings']['DateTitle']
+        self.node_entity_type_mappings = InputDataHandler.get_node_entity_type_mappings()
+
+        self.log_processed_records = (config['InputDataSettings']['LogProcessedRecords'] == 'True')
+
+
+        self.logger.info("InputDataHandler initialized.")
+
+    
+    def process_file(self, input_data_file_path=None):
+        """
+        Starts processing the input file and does the graph generation pipeline (entity extraction, pos extraction, relationship generation, domain words extraction and graph generation)
+        master_data_file_path : is an optional file for graph creation
+        """
+        self.logger.info("process_file is called.")
+
+        if input_data_file_path == None:
+            input_data_file_path = path.join(path.dirname(path.abspath(__file__)), self.config['InputDataSettings']['InputDataSetFile'])
+
+        if str.strip(input_data_file_path) == '':
+            raise Exception("Input Data File Path is unavailable. Please pass the value as input or set the value in the configuration.")
+
+        self.logger.info(str.format("input_data_file_path: {}", input_data_file_path))
+
+        #automatically extract the domain words and actions
+        domain_words_dict = self.domain_words_extractor.extract_domain_words(input_data_file_path, False, True)
+        self.logger.info("finished extracting domain words.")
+        #converting the list to a set for better performance while checking.
+        self.domain_verbs_set = set(domain_words_dict['VerbsList'])
+        self.domain_nouns_set = set(domain_words_dict['NounsList'])
+
+        #set up schema for graph here
+        self.setup_schema(domain_words_dict)
+
+        #read the file with pandas and loop through
+
+        df = pd.read_csv(input_data_file_path)
+        #The below line is used for testing and TESTING ONLY.
+        #df = pd.read_csv(path.join(path.dirname(path.abspath(__file__)),'data\\raw_partner_headlines_micro.csv'))
+
+        self.logger.info(str.format("read the input file. Row count: {}", len(df)))
+
+        for index, row in df.iterrows():
+            try:
+                content = row[self.content_title]
+                date = row[self.date_title]
+                #extract the entities
+                entities = self.entity_extractor.get_entities_bert(content)
+
+                #extract the pos
+                pos_tags = self.pos_extractor.get_pos_sentence(content)
+
+                #order them by indexes and see if they match the pattern
+                #also get the relevant words only as part of this
+                is_valid, ordered_word_list = self.pattern_finder.is_acceptable_pattern(entities, pos_tags, self.domain_nouns_set)
+
+                if is_valid == False:
+                    continue
+
+                #match the nouns & verbs with the synonyms dictionary for relationships
+                ##### NOT DONE FOR NOW ####
+                
+                #if pattern is good, then create the graph - node, relationship (ignore if already exists)
+                #create the entity, verb, noun connections in files
+                self.process_tokens_with_graph(ordered_word_list, date)
+
+                if self.log_processed_records:
+                    self.logger.info(str.format('processed: {}', ordered_word_list))
+
+                if index % 1000 == 0:
+                    self.logger.info(str.format('processed records count: {}', index))
+
+            except Exception as ex:
+                self.logger.info(str.format("ERR: Error processing row {}. Content: {}", index, row[self.content_title]))
+                self.logger.debug(str.format("Error processing row {}. Content: {}", index, row[self.content_title]))
+                self.logger.error('Error:', exc_info=ex)
+                continue
+            
+
+    def process_tokens_with_graph(self, ordered_word_list, date):
+        """
+        Splits the ordered word list along with their types (E,V,N), checks them and creates them in the graph
+        WORDS ONLY IN THE DOMAIN DICTIONARY will be considered for creation.
+        """
+        # get the verb, check against domain dictionary, and if exists, get its relationship name.
+        # if the verb does not exist in the dictionary, continue.
+        # get the entity / noun BEFORE the verb, get the entity type, get the id if already exists, or add this entity and get the id.
+        # get the entity(s) / noun(s) AFTER the verb, get the entity type, get the id if already exists, or add this entity and get the id.
+        # add the relationship between the first and the latter ones. 
+        # PROS and CONS - considers only the first VERB.
+
+        verb_tokens = list(filter(lambda x: x['type']=='V', ordered_word_list))
+        if len(verb_tokens) == 0:
+            return
+        
+        first_verb_token = verb_tokens[0]
+        if first_verb_token['lemma'] not in self.domain_verbs_set: # important to use LEMMA to check against the domain verbs set.
+            return
+
+        relationship_name = GraphGenerator.get_relationship_name(first_verb_token['lemma']) # THIS 'lemma' IS IMPORTANT.
+        occurrence_date = datetime.fromisoformat(date)
+        relationship_attributes = {  "happened": date, "month": occurrence_date.month, "year" : occurrence_date.year} 
+        
+        subject_part_tokens = []
+        object_part_tokens = []
+        verb_read = False
+
+        for token in ordered_word_list:
+            if token['type'] == 'V':
+                verb_read = True
+                continue
+            elif token['type'] == 'N' and verb_read == True:
+                object_part_tokens.append(token)
+            elif token['type'] == 'N' and verb_read == False:
+                subject_part_tokens.append(token)
+            elif token['type'] == 'E' and verb_read == True:
+                object_part_tokens.append(token)
+            elif token['type'] == 'E' and verb_read == False:
+                subject_part_tokens.append(token)
+        
+        for subject_token in subject_part_tokens:
+            #check and get the subject node / id from the graph for mapping.
+            #if it does not exist, create it.
+            subject_node_type = self.get_node_type(subject_token)
+            subject_node = self.graph_generator.add_node_to_graph(subject_token, subject_node_type)
+
+            for object_token in object_part_tokens:
+                #check and get the object node / id from the graph for mapping.
+                #if it does not exist, create it.
+                object_node_type = self.get_node_type(object_token)
+                object_node = self.graph_generator.add_node_to_graph(object_token, object_node_type)
+
+                #add the relationship between the subject and object nodes
+                self.graph_generator.add_relationship_to_graph(subject_node_type, subject_node, relationship_name, object_node_type, object_node, relationship_attributes)
+                if self.log_processed_records:
+                    self.logger.info(str.format('{}|{}({}) --> {} --> {}|{}({})', subject_token, subject_node, subject_node_type, relationship_name, object_token, object_node,  object_node_type))
+
+    def setup_schema(self, domain_words_dict):
+        """
+        sets up the schema with graph by executing relevant queries from the domain words dictionary
+        """
+        #get nodes information
+        nodes_list = self.nodes_list
+        node_infos = [ {'name': w} for w in nodes_list ]
+        #get relationships information
+        verbs_list = domain_words_dict['VerbsList']
+        relationship_infos = [ {'name': w} for w in verbs_list ]
+        #set up schema with graph
+        self.graph_generator.setup_schema(node_infos, relationship_infos)
+        self.logger.info("finished setting up schema from domain.")
+
+
+    def get_node_type(self, token):
+        """
+        this method returns the node type for the token based on the entity type or pos type.
+        """
+        # Entity --> B-ORG, I-ORG = Organization, B-PER, I-PER = Person, B-MISC, I-MISC = Object, B-LOC, I-LOC = Location
+        # POS --> VERB = edge, NOUN = Object
+        node_type = ''
+
+        if (token['type'] == 'E'):
+            node_type = self.node_entity_type_mappings[token['type']][token['entity']]
+        elif token['type'] == 'N':
+            node_type = 'Object'
+        
+        return node_type
+
+
+    def get_node_entity_type_mappings():
+        """
+        returns the full mapping of node type and entity type
+        """
+        return {
+            "E": {
+                "B-ORG": "Organization",
+                "I-ORG" : "Organization",
+                "B-PER": "Person",
+                "I-PER" : "Person",
+                "B-MISC": "Object",
+                "I-MISC" : "Object",
+                "B-LOC": "Location",
+                "I-LOC" : "Location",
+            },
+            "N" : "Object"
+        }
+
+        
+
+        
