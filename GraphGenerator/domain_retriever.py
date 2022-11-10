822a6876bfcc529c589193852e4a10360815d6b8
ManashJKonwar <rickykonwar@gmail.com>
2022-11-10 21:53:22 +0530

    Enhancement: Setting_up_domain_words_extraction
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- GraphGenerator/domain_retriever.py
+++ GraphGenerator/domain_retriever.py
@@ -7,11 +7,17 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import os
 import nltk
 import spacy
+import pandas as pd
 
+from tqdm import tqdm
+from collections import Counter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+
+from utility import read_config, write_yaml
 
 # List of symbols to not consider for tokenization
 symbol_list = ['`','~', '!','@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '[', '}', ']', '|', '\\', ':', ';', '"', '\'',',', '<', '.', '>', '?', '/' ]
@@ -53,10 +59,13 @@ class DomainRetriever:
         self._logger.info('GG: Domain retriever based stopwords loaded successfully')
 
         # Configuration based extraction
-        self._content_name = self._master_config['dataset_configurations']['content_name']
+        self._content_col_name = self._master_config['dataset_configurations']['content_col_name']
+        self._date_col_name = self._master_config['dataset_configurations']['date_col_name']
         self._most_common_words_count = int(self._master_config['dataset_configurations']['most_common_words_count'])
-        self._dump_domain_word_lists = (self._master_config['dataset_configurations']['dump_domain_words'] == 'True')
-        self._preload_domain_word_lists = (self._master_config['dataset_configurations']['preload_domain_words'] == 'True')
+        self._use_optimal_most_common_count = (self._master_config['dataset_configurations']['use_optimal_most_common_count'] == True)
+        self._common_word_coverage_score = float(self._master_config['dataset_configurations']['common_word_coverage_score'])
+        self._dump_domain_word_lists = (self._master_config['dataset_configurations']['dump_domain_words'] == True)
+        self._preload_domain_word_lists = (self._master_config['dataset_configurations']['preload_domain_words'] == True)
         self._domain_collection_path = self._master_config['dataset_configurations']['extra_domain_words_path']
         self._logger.info('GG: Domain retriever based configurations loaded successfully')
 
@@ -80,3 +89,94 @@ class DomainRetriever:
             spacy_model = spacy.load(model_name, exclude=excluded_steps)
         finally:
             return spacy_model
+
+    def get_optimal_most_common_count(self, word_counter : Counter):
+        """
+        This method decides the optimal most common count for the word counter
+        To cover 70% of the word set
+        """
+        self._logger.info("GG: Domain word extractor: optimal common counter started")
+
+        total_occurrences = sum([w[1] for w in word_counter.most_common()])
+        current_occurrences = 0
+        current_count = 0
+        for w in word_counter.most_common():
+            current_occurrences += w[1]
+            current_count += 1
+            if(float(current_occurrences / total_occurrences) > self._common_word_coverage_score):
+                break
+
+        self._logger.info("GG: Domain word extractor: optimal common counter completed successfully")
+        self._logger.info(str.format("Coverage: total_occurrences:{}, covered_occurrences:{}, total_count of words:{}, considered count: {}", total_occurrences, current_occurrences, len(word_counter), current_count))
+
+        return current_count
+
+    def domain_words_extractor(self, input_file_path=None):
+        """
+        This method helps extract domain specific words and form an resultant dictionary at the end.
+        """
+        self._logger.info('GG: Domain word extraction started')
+
+        def preload_domain_words():
+            if os.path.exists(self._domain_collection_path):
+                data = read_config(config_file_path=self._domain_collection_path)
+                
+                self._logger.info('GG: Preloading of domain words extracted')
+                return data
+
+        if self._preload_domain_word_lists:
+            return preload_domain_words()
+
+        df_input = pd.read_csv(input_file_path)
+        domain_words_dict, final_domain_words_dict = {'nouns':[], 'verbs':[], 'propernouns':[]}, {'nouns':[], 'verbs':[], 'propernouns':[]}
+
+        # Extracting verbs, nouns and propernouns from textual content
+        for index, row_data in tqdm(df_input.iterrows(), total=df_input.shape[0], desc='Extracting domain words'):
+            try:
+                text_content = row_data[self._content_col_name].lower()
+                tokenized_content = [w for w in word_tokenize(text_content) if not w in self._stop_words and not w in symbol_list]
+
+                # Extracting POS information for toeknized content
+                pos_content = self._nlp_pipeline(' '.join(tokenized_content))
+
+                # Extracting only nouns, verbs and proper nouns from POS information for each token
+                for token in pos_content:
+                    if token.pos_.__eq__('NOUN'):
+                        domain_words_dict['nouns'].append(token.text)
+                    elif token.pos_.__eq__('PROPN'):
+                        domain_words_dict['propernouns'].append(token.text)
+                    elif token.pos_.__eq__('VERB'):
+                        domain_words_dict['verbs'].append(token.text)
+
+            except Exception as ex:
+                print(ex)
+                self._logger.error('Caught exception while extracting domain specific words: %s' %(str(ex)))
+                continue
+        self._logger.info('GG: POS data of domain specific words extracted successfully')
+        
+
+        # Extract the counter for each word in the domain dict
+        verb_counter = Counter(domain_words_dict['verbs'])
+        noun_counter = Counter(domain_words_dict['nouns'])   
+        propnoun_counter = Counter(domain_words_dict['propernouns'])
+
+        # Extract the optimal counted list
+        optimal_most_common_noun_count = self.get_optimal_most_common_count(noun_counter) if self._use_optimal_most_common_count else self._most_common_words_count
+        optimal_most_common_propernoun_count = self.get_optimal_most_common_count(propnoun_counter) if self._use_optimal_most_common_count else self._most_common_words_count
+        optimal_most_common_verb_count = self.get_optimal_most_common_count(verb_counter) if self._use_optimal_most_common_count else self._most_common_words_count
+        self._logger.info('GG: Optimal Counter for POS data information of domain specific words is applied successfully')
+
+        # Retain only those words which justifies the common word counter score
+        final_domain_words_dict['nouns'] = [w[0] for w in noun_counter.most_common(optimal_most_common_noun_count)]
+        final_domain_words_dict['propernouns'] = [w[0] for w in propnoun_counter.most_common(optimal_most_common_propernoun_count)]
+        final_domain_words_dict['verbs'] = [w[0] for w in verb_counter.most_common(optimal_most_common_verb_count)]
+        self._logger.info('GG: Final domain specific words are extracted successfully')
+
+        # Save the domain specific dictionary as yaml file
+        if self._dump_domain_word_lists:
+            write_yaml(file_path='domain_related_words.yaml', data_dict=final_domain_words_dict)
+            self._logger.info('GG: Final domain specific words file written successfully')
+
+        self._logger.info('GG: Domain word extraction completed successfully')
+
+        return final_domain_words_dict
--- GraphGenerator/graph_operator.py
+++ GraphGenerator/graph_operator.py
@@ -7,6 +7,8 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import copy
+
 from entity_extractor import EntityExtractor
 from pos_identifier import PosIdentifier
 from domain_retriever import DomainRetriever
@@ -37,10 +39,78 @@ class GraphOperator:
 
         self._master_config = master_config
         self._logger = logger
+        
+        # Graph Operator supporting modules
         self._entity_extractor = entity_extractor
         self._pos_identifier = pos_identifier
         self._domain_retriever = domain_retriever
         self._pattern_finder = pattern_finder
         self._graph_generator = graph_generator
+        self._logger.info('GG: Graph operator supporting modules loaded successfully')
+
+        # Configuration based extraction / Graph specific variables
+        self._dump_domain_words = (self._master_config['dataset_configurations']['dump_domain_words'] == 'True')
+        self._content_col_name = self._master_config['dataset_configurations']['content_col_name']
+        self._graph_nodes = ['Person', 'Organization', 'Object', 'Location']
+        self._domain_words = {'nouns':[], 'verbs':[], 'propnouns':[]}
+        self._node_entity_mapping = self._extract_node_entity_mapping()
 
         self._logger.info('GG: Graph operator based initialized successfully')
+
+    def _extract_node_entity_mapping(self) -> dict():
+        """
+        This method extracts full mapping of node type and entity type
+
+        returns:
+        Dictionary of E and N based items for specific patterns
+        """
+        return {
+            "E": {
+                "B-ORG": "Organization",
+                "I-ORG" : "Organization",
+                "B-PER": "Person",
+                "I-PER" : "Person",
+                "B-MISC": "Object",
+                "I-MISC" : "Object",
+                "B-LOC": "Location",
+                "I-LOC" : "Location",
+            },
+            "N" : "Object"
+        }
+
+    def process_input(self) -> None:
+        """
+        This method starts the file processing and calls the entire pipeline starting with entity extraction, pos identifier, domain word retriever and graph generation
+        """
+        try:
+            # Extracting input file path from master configuration
+            input_file_path = self._master_config['dataset_configurations']['data_file_path'] if self._master_config['dataset_configurations']['data_file_path'] != '' else None
+            if input_file_path is None:
+                self._logger.error('Input file path is not existing. Please provide and retry again')
+                return
+
+            # Extracting domain specific terms
+            self._domain_words = self._domain_retriever.domain_words_extractor(input_file_path=input_file_path)
+
+            # Converting domain specific terms datastructures to set for easy computation
+            self._domain_nouns_set = set(self._domain_words['nouns'])
+            self._domain_verbs_set = set(self._domain_words['verbs'])
+            
+            # Setting up graoh schema
+            self.setup_graph_schema()
+
+        except Exception as ex:
+            print(ex)
+            self._logger.error('Caught error while processing the input data: %s' %(str(ex)))
+
+    def setup_graph_schema(self):
+        """
+        This method is responsible for executing queries from domain words extracted and setting up the graph DB
+        """
+        # Selected graphical nodes to be created
+        node_list = copy.deepcopy(self._graph_nodes)
+        node_infos = [{'name':entity_name} for entity_name in node_list]
+        
+        # Selected graphical relationships to be created
+        verb_list = copy.deepcopy(self._domain_words['verbs'])
+        relationship_infos = [{'name':relationship_name} for relationship_name in verb_list]
